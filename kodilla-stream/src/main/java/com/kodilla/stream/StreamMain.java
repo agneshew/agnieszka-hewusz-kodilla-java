2aed44621875957895ed6c5bd349597b1391e2cb
agneshew <agnes.hewusz@gmail.com>
2020-07-20 18:17:52 +0200

    Zadanie 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 import static com.sun.tools.javac.util.StringUtils.toUpperCase;
 
@@ -12,6 +12,10 @@ public class StreamMain {
         poemBeautifier.beautify("Zosia", text -> "!!!" + text);
         poemBeautifier.beautify("Zosia", text -> text + ":)");
         poemBeautifier.beautify("Zosia", text -> toUpperCase(text));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
 
     }
 }
