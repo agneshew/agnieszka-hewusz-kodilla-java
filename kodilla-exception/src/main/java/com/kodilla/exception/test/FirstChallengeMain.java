7bcdf7524999846f4b7b03c1c01a954354a054d4
agneshew <agnes.hewusz@gmail.com>
2020-07-28 15:44:36 +0200

    Zadanie 8.3

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallengeMain.java
@@ -0,0 +1,14 @@
+package com.kodilla.exception.test;
+
+public class FirstChallengeMain {
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        double result = firstChallenge.divide(3, 0);
+
+        System.out.println(result);
+
+    }
+}
+
