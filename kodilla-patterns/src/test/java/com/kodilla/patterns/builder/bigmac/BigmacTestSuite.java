21958ec69308f5e5eea36ba84292bf07e26d1488
agneshew <agnes.hewusz@gmail.com>
2020-08-21 10:48:10 +0200

    Zadanie 11.5

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,28 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+    @Test
+    public void testPizzaNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun("Sesame")
+                .burgers(2)
+                .sauce("Barbecue")
+                .ingredient("Lettuce")
+                .ingredient("Onion")
+                .ingredient("Cucumber")
+                .ingredient("Cheese")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(4, howManyIngredients);
+        Assert.assertEquals("Sesame", bigmac.getBun());
+        Assert.assertEquals(2, bigmac.getBurgers());
+        Assert.assertEquals("Barbecue", bigmac.getSauce());
+    }
+}
