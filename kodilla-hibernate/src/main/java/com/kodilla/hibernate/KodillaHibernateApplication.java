d29adf7f2237d066ce723633972cc2c244f25757
agneshew <agnes.hewusz@gmail.com>
2020-09-16 10:13:43 +0200

    Zadanie 13.1

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -1,4 +1,13 @@
 package com.kodilla.hibernate;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class KodillaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibernateApplication.class, args);
+    }
+
 }
